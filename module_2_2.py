4276863545757fb15b724af92e9c6accd635f85e
kirekat <kec85@mail.ru>
2024-09-08 17:10:38 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+number_first= input('Введите первое число: ')
+number_second= input('Введите второе число: ')
+number_third= input('Введите третье число: ')
+if number_first == number_second == number_third:
+    print(3)
+elif number_first != number_second != number_third != number_first:
+    print(0)
+else:
+    print(2)
